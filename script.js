5bf92fe01b9e2b3a70186c4d6dd2659544a70565
Anuhas88 <hirunanuhas954@gmail.com>
2025-05-09 17:40:10 +0530

    Last Update

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
-const taskInput =document.getElementById('TaskInput');
+
+const taskInput = document.getElementById('taskInput');
 const addTaskBtn = document.getElementById('addTaskBtn');
 const taskList = document.getElementById('taskList');
 const filterAllBtn = document.getElementById('filterAll');
 const filterCompleteBtn = document.getElementById('filterCompleted');
 const filterPendingBtn = document.getElementById('filterPending');
 
-let tasks = [];
-function renderTask(filterTasks = tasks){
-    taskList.innerHTML = "";
-    filterTasks.forEach((Task,index) => {
-        const li = document.createElement("li");
-        li.classList.toggle("completed",Task.completed);
+let tasks = [];   
 
-        const taskText = document.createElement("span");
+function renderTask(filterTasks = tasks){
+
+    taskList.innerHTML = "";
+   
+    filterTasks.forEach((task,index)=>{
+         
+        const li = document.createElement("li");
+    
+        li.classList.toggle("completed", task.completed);
+
+        const taskText = document.createElement("span");  
         taskText.textContent = task.name;
         li.appendChild(taskText);
 
-        li.onclick = () => toggleTaskComplete(index);
-        
+        li.onclick = () => toggleTaskComplete(index);   
         const deleteBtn = document.createElement("button");
         deleteBtn.textContent = "Delete";
+                                
         deleteBtn.onclick = () => deleteTask(index);
-        li.appendChild(deleteBtn);
+        li.appendChild(deleteBtn);    
 
         taskList.appendChild(li);
     })
 }
 
 function addTask(){
-    const taskName = taskInput.ariaValue.trim();
-    if(tasks !== ""){
+    const taskName = taskInput.value.trim();
+    if(taskName !== ""){
         let task = {
             name: taskName,
             completed: false
@@ -40,17 +46,32 @@ function addTask(){
         renderTask();
     }
 
-    function toggleTaskComplete(index){
-        tasks[index].completed = !tasks[index].completed;
-        renderTask();
-    }
-    addTaskBtn.addEventListener('click',addTask);
+}
 
-    filterAllBtn.addEventListener('click',() => renderTask());
-        
-    filterCompleteBtn.addEventListener('click',() =>
-            renderTask(tasks.filter(task => task.completed)));
-    filterPendingBtn.addEventListener('click',() =>
-        renderTask(tasks.filter(task => !task.completed)));
+function deleteTask(index){
+
+    tasks.splice(index,1);
+    renderTask();
+
+}
+
+function toggleTaskComplete(index){
+   
+    tasks[index].completed = !tasks[index].completed;
     renderTask();
 }
+
+
+addTaskBtn.addEventListener('click',addTask);
+
+filterAllBtn.addEventListener('click', () => renderTask());  
+filterCompleteBtn.addEventListener('click',() =>
+     renderTask(tasks.filter(task => task.completed)));  
+filterPendingBtn.addEventListener('click',() =>
+    renderTask(tasks.filter(task => !task.completed))); 
+renderTask();
+
+
+
+
+
